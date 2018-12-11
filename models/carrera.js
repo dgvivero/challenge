22b3c81c8857ae5d305b18e87ead895f6a4b9df9
Germán Vivero <german.vivero@SMEMPRESARIAL.LAN>
2018-12-11 09:44:25 -0300

    Change compose and add supervisor to watch changes on files

--- models/carrera.js
+++ models/carrera.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const CarreraSchema = new Schema({
-    nombre          : {type:String, required: true,  unique: true},
+    nombre          : {type:String, required: true, unique: true},
     titulo          : String
 });
 
